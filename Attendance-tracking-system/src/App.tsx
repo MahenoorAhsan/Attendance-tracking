e657e2846b3fc867f6ef6a1ff1420f4cb1cb72bf
Mahenoor Ahsan <mahii828113@gmail.com>
2025-02-21 23:05:39 +0530

    project base

---
+++ Attendance-tracking-system/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import styles from './App.css'
+import {Login} from './components/Login/Login'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <Login></Login>
+   </div>
+  )
+}
+
+export default App
